86104627301e4e6a6430a35e00e07298d2a8a8ab
Maruf343 <mm3677748@gmail.com>
2024-02-18 12:28:06 +0600

    this my first commit

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+
+$FirstName= "Abdullah";
+$LastName= "Al Maruf";
+// $Age= "22";
+// echo $FirstName." ".$LastName;
+echo "My name is:".$FirstName."".$LastName;
+// echo ",My age is:".echo "My name is:".$FirstName."".$LastName;
+// echo ",My age is:".
+
+// $cars = array("volvo","ford","toyota");
+// var_dump($cars);
+// echo $cars[2];
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    
+        $FirstName= "Abdullah";
+        $LastName= "Al Maruf";
+        $Age= "22";
+        echo "My name is:".$FirstName." ".$LastName;
+        echo " ,My age is:".$Age;
+    ?>
+
+</body>
+</html>
